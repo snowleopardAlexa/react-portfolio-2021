0d352ae5965a8cc18f7d840a43e28a0081317e70
snowleopardAlexa <alexaslomski@gmail.com>
2021-11-17 21:58:01 -0500

    add toggle

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import Intro from "./components/Intro/Intro";
 import About from "./components/About/About";
 import ProductList from "./components/ProductList/ProductList";
 import Contact from "./components/Contact/Contact";
+import Toggle from "./components/Toggle/Toggle";
 
 const App = () => {
     return (
       <div>
+       <Toggle />
        <Intro />
        <About />
        <ProductList />
