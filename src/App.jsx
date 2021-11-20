1803f9ab4c626bb194c0be6b731a2da67dcedb0a
snowleopardAlexa <alexaslomski@gmail.com>
2021-11-19 20:26:37 -0500

    add produclist to app js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Intro from "./components/Intro/Intro";
 import About from "./components/About/About";
+import ProductList from "./components/ProductList/ProductList";
 
 const App = () => {
     return (
       <div>
        <Intro />
        <About />
+       <ProductList />
       </div>
     )
 };
