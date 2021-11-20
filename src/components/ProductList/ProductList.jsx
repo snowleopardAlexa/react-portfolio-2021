ee1ce0f003546345d9ed546e6a79cfc1b170ce9b
snowleopardAlexa <alexaslomski@gmail.com>
2021-11-19 20:23:32 -0500

    add html

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,9 +4,14 @@ import './productlist.css';
 
 const ProductList = () => {
     return (
-        <div>
-card
-        </div>
+       <div className="pl">
+           <div className="pl-texts">
+               <h1 className="pl-title">Create and inspire</h1>
+               <p className="pl-desc">
+                   Alexa is developer... 
+               </p>
+           </div>
+       </div>
     )
 }
 
