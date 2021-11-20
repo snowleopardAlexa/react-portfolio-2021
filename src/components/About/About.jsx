5af7e73de5fcca1cf3fbd3b12fcf87a9d7573d30
snowleopardAlexa <alexaslomski@gmail.com>
2021-11-19 20:08:10 -0500

    add about me html and styles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './about.css';
-import snowleopard from "../../img/snowleopard.jpg";
+import snowleopard from "../../img/snow.jpg";
 
 const About = () => {
     return (
@@ -15,7 +15,26 @@ const About = () => {
                     />
                 </div>
             </div>
-            <div className="about-right"></div>
+            <div className="about-right">
+                <h1 className="a-title">About Me</h1>
+                <p className="a-sub">
+                    It is aaa tex....
+                </p>
+                <p className="a-desc">
+                    lorem ipsum dolori....
+                </p>
+                <div className="a-award">
+                    <img src={snowleopard} alt="" className="a-award-img" />
+                    <div className="a-award-texts">
+                        <h4 className="a-award-title">
+                            international design
+                        </h4>
+                        <p className="a-award-desc">
+                            none ems spususus
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
