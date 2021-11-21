8338683d6648278bacefe4571d90bbf4227e2bce
snowleopardAlexa <alexaslomski@gmail.com>
2021-11-21 18:52:11 -0500

    add styling

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -8,6 +8,13 @@ const Intro = () => {
               <div className="intro-left-wrapper">
                   <h1 className="i-intro">Hello, I'm Alexa!</h1>
                   <h2 className="i-name">but my name is Aleksandra Slomska</h2>
+                  <div className="i-title">
+                      <div className="i-title-item">React Developer</div>
+                      <div className="i-title-item">Front-End Developer</div>
+                      <div className="i-title-item">Pet Lover</div>
+                      <div className="i-title-item">Proud Moma of a fur-baby Kion The Lion King</div>
+                      <div className="i-title-item">Prose & Poetry Writer</div>
+                  </div>
               </div>
           </div>
           <div className="intro-right">right</div>
