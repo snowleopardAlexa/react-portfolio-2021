5a3c7a480187d7ac0e33208b0710b2f0303d6369
snowleopardAlexa <alexaslomski@gmail.com>
2021-11-21 18:43:25 -0500

    add name

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -4,7 +4,12 @@ import "./intro.css";
 const Intro = () => {
     return (
       <div className="intro">
-          <div className="intro-left">left</div>
+          <div className="intro-left">
+              <div className="intro-left-wrapper">
+                  <h1 className="i-intro">Hello, I'm Alexa!</h1>
+                  <h2 className="i-name">but my name is Aleksandra Slomska</h2>
+              </div>
+          </div>
           <div className="intro-right">right</div>
       </div>
     )
