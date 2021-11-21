a8e9b6bc0b67c394556ee0aa14fbd524757e9ac2
snowleopardAlexa <alexaslomski@gmail.com>
2021-11-20 19:56:32 -0500

    add cards and styling

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './about.css';
+import snowleopard from "../../img/snowleopard.jpg";
 
 const About = () => {
     return (
         <div className="about">
-            <div className="about-left"></div>
+            <div className="about-left">
+                <div className="a-card bg"></div>
+                <div className="a-card">
+                    <img
+                      src={snowleopard}
+                      alt=""
+                      className="a-img"
+                    />
+                </div>
+            </div>
             <div className="about-right"></div>
         </div>
     )
