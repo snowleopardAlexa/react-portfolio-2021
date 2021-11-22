fa63e6a2d7ba549113f8e31d43bd6c340aebbff7
snowleopardAlexa <alexaslomski@gmail.com>
2021-11-21 19:08:45 -0500

    add desc and styles

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -9,11 +9,16 @@ const Intro = () => {
                   <h1 className="i-intro">Hello, I'm Alexa!</h1>
                   <h2 className="i-name">but my name is Aleksandra Slomska</h2>
                   <div className="i-title">
+                      <div className="i-title-wrapper">
                       <div className="i-title-item">React Developer</div>
                       <div className="i-title-item">Front-End Developer</div>
                       <div className="i-title-item">Pet Lover</div>
                       <div className="i-title-item">Proud Moma of a fur-baby Kion The Lion King</div>
                       <div className="i-title-item">Prose & Poetry Writer</div>
+                      </div>
+                  </div>
+                  <div className="i-desc">
+                      I am Front-End/React Developer that is aspiring to become Full Stack Developer.
                   </div>
               </div>
           </div>
