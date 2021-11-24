06fcabb657b73c95d7f91214622b67da0c1db35a
snowleopardAlexa <alexaslomski@gmail.com>
2021-11-23 20:35:22 -0500

    add links to img trial

--- src/data.js
+++ src/data.js
@@ -1,32 +1,33 @@
+
 export const products = [
     {
         id: 1,
-        img: "",
-        link: "https://www.google.com/search?q=book&sxsrf=AOaemvK-KJCvELOWV0cTbLAkJ-Wj6N_jSQ:1637546520935&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-pbuV8Kr0AhURpXIEHRBnDWsQ_AUoAXoECAIQAw&biw=1202&bih=879&dpr=2#imgrc=FEJeVtZf8WlsIM",
+        img: "https://i.ibb.co/4TD6CYF/1.jpg",
+        link: "http://lama.dev",
     },
     {
         id: 2,
-        img: "",
-        link: "https://www.pexels.com/photo/woman-in-white-dress-walking-on-the-street-10161997/",
+        img: "https://i.ibb.co/4TD6CYF/1.jpg",
+        link: "http://lama.dev",
     },
     {
         id: 3,
-        img: "",
-        link: "https://www.pexels.com/photo/woman-in-white-dress-walking-on-the-street-10161997/",
+        img: "https://i.ibb.co/4TD6CYF/1.jpg",
+        link: "http://lama.dev",
     },
     {
         id: 4,
-        img: "",
-        link: "https://www.pexels.com/photo/woman-in-white-dress-walking-on-the-street-10161997/",
+        img: "https://i.ibb.co/4TD6CYF/1.jpg",
+        link: "http://lama.dev",
     },
     {
-        id: 4,
-        img: "",
-        link: "https://www.pexels.com/photo/woman-in-white-dress-walking-on-the-street-10161997/",
+        id: 5,
+        img: "https://i.ibb.co/4TD6CYF/1.jpg",
+        link: "http://lama.dev",
     },
     {
-        id: 4,
-        img: "",
-        link: "https://www.pexels.com/photo/woman-in-white-dress-walking-on-the-street-10161997/",
+        id: 6,
+        img: "https://i.ibb.co/4TD6CYF/1.jpg",
+        link: "http://lama.dev",
     },
 ]
