92fe98c4a5dbc15daa2f2873279375f8c28c1fe4
snowleopardAlexa <alexaslomski@gmail.com>
2021-11-27 18:27:03 -0500

    add technologies

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -1,52 +1,15 @@
 import React from 'react';
 import './technologies.css';
-//import js from "../../img/tech-icons/js.png";
 
-const Technologies = () => {
+   
+const Technologies = ({img, title }) => {
     return (
-        <React.Fragment>
-        <h1 className="tech-title">Technologies</h1>
-        <h1 className="tech-subtitle">These are technologies I am using</h1>
-        <div className="tech">
-            <div className="tech-left">
-            <div className="t-award">
-                  
-                </div>
-            </div>
-            <div className="tech-right">
-                <div className="t-award">
-                    <img alt="" className="t-award-img" />
-                    <div className="t-award-texts">
-                        <h4 className="t-award-title">
-                            international design
-                        </h4>
-                        <p className="t-award-desc">
-                            none ems spususus
-                        </p>
-                    </div>
-                    <img  alt="" className="t-award-img" />
-                    <div className="t-award-texts">
-                        <h4 className="t-award-title">
-                            international design
-                        </h4>
-                        <p className="t-award-desc">
-                            none ems spususus
-                        </p>
-                    </div>
-                    <img  alt="" className="t-award-img" />
-                    <div className="t-award-texts">
-                        <h4 className="t-award-title">
-                            international design
-                        </h4>
-                        <p className="t-award-desc">
-                            none ems spususus
-                        </p>
-                    </div>
-                </div>
-            </div>
+        <div className="technologies">
+            <img src={img} alt="" className="p-img" />
+            <p className="p-title">{title}</p>
         </div>
-        </React.Fragment>
-    )
-}
+        );
+    }
+ 
 
 export default Technologies;
