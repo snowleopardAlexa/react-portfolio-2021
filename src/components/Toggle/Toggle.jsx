01ea555a50283489114e078b2234e675b9ca7ae8
snowleopardAlexa <alexaslomski@gmail.com>
2021-11-26 21:47:26 -0500

    add icons sun and moon

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './toggle.css';
+import sun from "../../img/sun.png";
+import moon from "../../img/moon.png";
 
 const Toggle = () => {
     return (
         <div className="toggle">
-
+            <img src={sun} alt="" className="t-icon"></img>
+            <img src={moon} alt="" className="t-icon"></img>
         </div>
     )
 }
