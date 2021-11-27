9967f0bf7eefd0fbd3b0c9c8f051d4e50daa7e24
snowleopardAlexa <alexaslomski@gmail.com>
2021-11-26 21:06:40 -0500

    add email send form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,6 +13,13 @@ const formRef = useRef()
 
 const handleSubmit = (e) => {
     e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_USER_ID')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
 }
 
     return (
