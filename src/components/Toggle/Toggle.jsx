35b3f1e31b693e0f69e34ae2120c0d5fbec989b3
snowleopardAlexa <alexaslomski@gmail.com>
2021-11-27 08:22:41 -0500

    add usecontext

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import './toggle.css';
 import sun from "../../img/sun.png";
 import moon from "../../img/moon.png";
+import { useContext } from 'react';
+import { ThemeContext } from '../../Context';
 
 const Toggle = () => {
+
+    const theme = useContext(ThemeContext)
+    
+    const handleClick = () => {
+        theme.dispatch({type: "TOGGLE"})
+    }
+
     return (
         <div className="toggle">
             <img src={sun} alt="" className="t-icon"></img>
             <img src={moon} alt="" className="t-icon"></img>
-            <div className="t-btn"></div>
+            <div className="t-btn" onClick={handleClick}></div>
         </div>
     )
 }
