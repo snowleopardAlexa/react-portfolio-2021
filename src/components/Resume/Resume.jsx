4af469e5475580ac8ca2d729b6494a1315a41878
snowleopardAlexa <alexaslomski@gmail.com>
2021-11-30 22:46:54 -0500

    write resume component

---
+++ src/components/Resume/Resume.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './resume.css';
+
+   
+const Resume = () => {
+    return (
+        <div className="resume">
+            
+        </div>
+        );
+    }
+ 
+
+export default Resume;
