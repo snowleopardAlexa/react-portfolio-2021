7e1966f6f38da384ac835168f9813af3894235af
snowleopardAlexa <alexaslomski@gmail.com>
2021-11-30 21:54:14 -0500

    add icons and change their color to red

--- src/data.js
+++ src/data.js
@@ -2,13 +2,19 @@ import alexa from "./img/alexa.jpg";
 import js from "./img/tech-icons/js.png";
 import react from "./img/tech-icons/react.png";
 import redux from "./img/tech-icons/redux.png";
-import node from "./img/tech-icons/node.png";
+import node from "./img/tech-icons/nodejs-icon.svg";
 import graphql from "./img/tech-icons/graphql.png";
-import mongodb from "./img/tech-icons/mongodb.png";
+import mongodb from "./img/tech-icons/mongodb.svg";
 import html from "./img/tech-icons/html.png";
 import css from "./img/tech-icons/css.png";
-import mongo from "./img/tech-icons/mongo.svg";
-import IconAmazonwebservices from 'react-devicon/amazonwebservices/original'
+import postman from "./img/tech-icons/postman.svg";
+import express from "./img/tech-icons/express.svg";
+import jest from "./img/tech-icons/jest.svg";
+import git from "./img/tech-icons/git.svg";
+import github from "./img/tech-icons/github.svg";
+import nextjs from "./img/tech-icons/next-js.svg";
+import npm from "./img/tech-icons/npm.svg";
+
 
 
 
@@ -63,47 +69,77 @@ export const technologies = [
 
     {
         id: 1,
-        img: IconAmazonwebservices,
+        img: js,
         title: "JavaScript",
     },
     {
         id: 2,
         img: react,
-        title: "JavaScript",
+        title: "React",
     },
     {
         id: 3,
         img: redux,
-        title: "JavaScript",
+        title: "Redux",
     },
     {
         id: 4,
         img: node,
-        title: "JavaScript",
+        title: "Node.js",
     },
     {
         id: 5,
         img: mongodb,
-        title: "JavaScript",
+        title: "Mongodb",
     },
     {
         id: 6,
         img: graphql,
-        title: "JavaScript",
+        title: "Graphql",
     },
     {
         id: 7,
         img: html,
-        title: "JavaScript",
+        title: "HTML",
     },
     {
         id: 8,
         img: css,
-        title: "JavaScript",
+        title: "CSS",
     },
     {
         id: 9,
-        img: mongo,
-        title: "JavaScript",
+        img: postman,
+        title: "Postman",
+    },
+    {
+        id: 10,
+        img: express,
+        title: "Express",
+    },
+    {
+        id: 11,
+        img: jest,
+        title: "Jest",
+    },
+    {
+        id: 12,
+        img: git,
+        title: "Git",
+    },
+    {
+        id: 12,
+        img: github,
+        title: "Github",
+    },
+    {
+        id: 13,
+        img: nextjs,
+        title: "Next.js",
+    },
+    {
+        id: 14,
+        img: npm,
+        title: "NPM",
     },
 ]
