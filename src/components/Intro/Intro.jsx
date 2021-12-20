5b2890b98aea6ca173f235979fcc7d5ceff4c57d
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-20 15:41:40 -0500

    add frontend porto btn

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -8,7 +8,7 @@ const Intro = () => {
           <div className="intro-left">
               <div className="intro-left-wrapper">
                   <h1 className="i-intro">Hello, I'm Alexa!</h1>
-                  <h2 className="i-name">but my name is Aleksandra Slomska</h2>
+                  <h2 className="i-name">and this is my React portoflio.</h2>
                   <div className="i-title">
                       <div className="i-title-wrapper">
                        <div className="i-title-item">I am . . .</div>
@@ -21,64 +21,10 @@ const Intro = () => {
                   <p className="i-desc">
                       I am Front-End/React Developer that is aspiring to become Full Stack Developer.
                   </p>
+                  <a href="https://snowleopardalexa.github.io/portfolio-2021/"><button className="front-end-portfolio">
+                      Go to Front-End Portfolio
+                  </button></a>
               </div>
-              {/* svg scroll button */}
-        <svg
-          width="75"
-          height="75"
-          viewBox="0 0 75 75"
-          fill="none"
-          stroke="#a00b0b"
-          className="i-scroll"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g id="scroll">
-            <path
-              id="Vector"
-              d="M40.5 15L34.5 9L28.5 15"
-              stroke-width="3"
-              stroke-miterlimit="10"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              id="Vector_2"
-              d="M28.5 24L34.5 30L40.5 24"
-              stroke-width="3"
-              stroke-miterlimit="10"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <g id="Group">
-              <path
-                id="Vector_3"
-                d="M9 37.5H60"
-                stroke-width="3"
-                stroke-miterlimit="10"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </g>
-            <path
-              id="Vector_4"
-              d="M34.5 27V9"
-              stroke-width="2.9895"
-              stroke-miterlimit="10"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <g id="Group_2">
-              <path
-                id="Vector_5"
-                d="M9 27C9 12.918 20.418 1.5 34.5 1.5C48.5859 1.5 60 12.918 60 27C60 29.8906 60 45.1094 60 48C60 62.082 48.5859 73.5 34.5 73.5C20.418 73.5 9 62.082 9 48C9 45.1094 9 29.8906 9 27Z"
-                stroke-width="3"
-                stroke-miterlimit="10"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </g>
-          </g>
-        </svg>
           </div>
           <div className="intro-right">
               <div className="i-bg"></div>
